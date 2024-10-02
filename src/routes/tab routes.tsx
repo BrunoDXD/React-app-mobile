f87b39fac1f50725b21327f8263f772d1a9bb36a
Bruno Silva <brunodxd09@gmail.com>
2024-10-01 21:26:59 -0300

    Configuração aba Perfil

--- src/routes/tab routes.tsx
+++ src/routes/tab routes.tsx
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Feather } from '@expo/vector-icons';
 import Inicio from '../screens/Inicio';
 import New from '../screens/New';
+import Perfil from '../screens/Perfil';
 
 const Tab = createBottomTabNavigator();
 
@@ -17,11 +18,19 @@ export default function TabRoutes() {
                 }}
             />
             <Tab.Screen
-                name="new"
+                name="Novo"
                 component={New}
                 options={{
                     tabBarIcon: ({ color, size }) => <Feather name="plus" color={color} size={size} />,
                     tabBarLabel: 'Novo'
+                }}
+            />
+            <Tab.Screen
+                name="Perfil"
+                component={Perfil}
+                options={{
+                    tabBarIcon: ({ color, size }) => <Feather name="user" color={color} size={size} />,
+                    tabBarLabel: 'Perfil'
                 }}
             />
         </Tab.Navigator>
--- src/screens/Perfil.tsx
+++ src/screens/Perfil.tsx
@@ -1,9 +1,59 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { BaseButton } from 'react-native-gesture-handler';
 
 export default function Perfil() {
+  const [valor1, setvalor1]       = useState('');
+  const [valor2, setvalor2]       = useState('');
+  const [resultado, setresultado] = useState(0);
+  const soma=()=>{
+    setresultado(parseFloat(valor1) + parseFloat(valor2));
+  };
+  const subtrair=()=>{
+    setresultado(parseFloat(valor1) - parseFloat(valor2));
+  };
+  const multiplicar=()=>{
+    setresultado(parseFloat(valor1) * parseFloat(valor2));
+  };
+  const dividir=()=>{
+    if(parseFloat(valor2)!=0){
+    setresultado(parseFloat(valor1) / parseFloat(valor2));}
+    else{
+      setresultado(NaN);
+    }
+  };
   return (
     <View style={styles.container}>
-     <Text style={styles.titulo}>Perfil</Text>
+     <Text style={styles.titulo}>Calcular</Text>
+     
+     <TextInput 
+        placeholder='Digite o primeiro número'
+        value        = {valor1}
+        style={styles.input}
+        onChangeText = {setvalor1}
+        keyboardType='numeric'
+     ></TextInput>
+     
+     <TextInput 
+        placeholder='Digite o segundo número'
+        value        = {valor2}
+        style={styles.input}
+        onChangeText = {setvalor2}
+        keyboardType='numeric'
+     ></TextInput>
+
+      <Button title="Somar" onPress={soma}/>
+      <Button title="Subtrair" onPress={subtrair}/>
+      <Button title="Multiplicar" onPress={multiplicar}/>
+      <Button title="Dividir" onPress={dividir}/>
+     
+     <Text 
+     style={styles.input}
+     >{resultado}</Text>
+     
+     
+     
+      
     </View>
   );
 }
@@ -12,11 +62,26 @@ const styles = StyleSheet.create({
   titulo: {
     fontSize: 32,
     fontWeight: 'bold',
+    color: '#11111',
+    textAlign: 'center',
+    padding: 10,
   },
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 0,
+    textAlign: 'center',
+    alignItems: 'center',
     },
+    input: {
+      height: 40,
+      borderColor: '#ccc',
+      borderWidth: 1,
+      marginBottom: 10,
+      padding: 10,
+      marginTop: 10,
+      width: '50%',
+      borderRadius: 5,
+      opacity: 0.5,
+    }
   });
